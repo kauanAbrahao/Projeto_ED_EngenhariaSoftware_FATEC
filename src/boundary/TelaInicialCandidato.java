042df366625c803b80550d7b0805886b0ef6bfbd
Guilherme-Sardinha <guilherme.sardinha@live.com>
2020-11-18 12:41:10 -0300

    vai

--- src/boundary/TelaInicialCandidato.java
+++ src/boundary/TelaInicialCandidato.java
@@ -1,22 +1,25 @@
 package boundary;
 
 import java.io.FileNotFoundException;
+import java.io.IOException;
 
 import javax.swing.JOptionPane;
 
 import control.VerificaProcessoController;
+import control.InscricaoProcessoController;
 import entities.Candidato;
 import entities.ListaCandidatos;
 
 public class TelaInicialCandidato {
 		
-	public void telaInicial(String cpfValidador) throws FileNotFoundException {
+	public void telaInicial(String cpfValidador) throws IOException {
 		
 		//Essas 3 linhas puxam as informações do txt e transformam em um lista. Ademais, puxa o candidato que logou no sistema
 		ListaCandidatos lista = new ListaCandidatos();
 		lista.buscaCandidatosNoTxt();
 		Candidato candidato = lista.buscaCandidatoPorCPF(cpfValidador);
 		VerificaProcessoController processo = new VerificaProcessoController();
+		InscricaoProcessoController inscreve = new InscricaoProcessoController();
 		
 		int opc = -1;
 		
@@ -25,7 +28,7 @@ public class TelaInicialCandidato {
 			"2 - Verificar Situação no Processo Seletivo" + "\n" + "9 - Retornar"));
 			
 			switch(opc) {
-				case 1: System.out.println("falta implementar");
+				case 1: inscreve.inscreveProcesso(candidato);
 				break;
 					
 				case 2: processo.verificaProcesso(candidato);
